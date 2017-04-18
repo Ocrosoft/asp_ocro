23061803db8f4569de89f818aaecd7eb6337d293
Ocrosoft <ocrosoft@ocrosoft.com>
2017-04-18 09:35:59 +0800

    三层架构准备；

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class Student
+    {
+        private string username { get; set; }
+        private string password { get; set; }
+        private string sex { set; get; }
+        private string grade { set;get; }
+        private string age { set; get; }
+        private string major { set; get; }
+        private string IP { set; get; }
+        private string regtime { set; get; }
+    }
+}
